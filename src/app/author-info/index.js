1c7b1ee048d28fc1b5d7c9baa4fdb9a6d88e74b0
Highoc <door0172@gmail.com>
2018-11-08 22:57:32 +0300

    clean repo

--- src/app/author-info/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-unused-vars */
-
-import FormGeolocation from '../../lib/components/form/-geoposition';
-import FormInput from '../../lib/components/form/-input';
-import FormFile from '../../lib/components/form/-file';
-import AuthorInfoForm from '../../lib/components/author-info-form';
